4564f99a5e5d8c780d90f3185c1b1c26dedbc275
wpdevelopment11 <wpdevelopment11@gmail.com>
2025-04-08 12:36:59 +0300

    File editing is supported now.
    Appropriate language will be inserted in a fenced code block.

--- codeblocks.py
+++ codeblocks.py
@@ -3,6 +3,9 @@ import sys
 import os.path
 
 from enum import Enum
+from tempfile import NamedTemporaryFile
+from shutil import copy
+from os import unlink
 
 class Codeblock(Enum):
     IN = 1
@@ -10,6 +13,7 @@ class Codeblock(Enum):
     OUT = 3
 
 DEFAULT_LANG = "txt"
+EDIT_FILES = True
 
 try:
     from magika import Magika
@@ -30,39 +34,61 @@ except ImportError:
         print("Magika or Guesslang is required to run this script. Install one of them to proceed!")
         sys.exit(1)
 
-
 if len(sys.argv) != 2:
     print("Usage: python codeblocks.py dir")
     sys.exit(1)
 
-result = open("result.txt", "w", encoding="utf-8")
-
 files = glob.iglob("**/*.md", root_dir=sys.argv[1], recursive=True)
+# newline="" is important. See below.
+temp = NamedTemporaryFile("w+", encoding="utf-8", newline="", delete=False)
 
 for file in files:
     fullpath = os.path.join(sys.argv[1], file)
     blockstate = Codeblock.OUT
     code = []
     fence = ""
-    for linenum, line in enumerate(open(fullpath, encoding="utf-8", newline=''), 1):
+    # The argument newline="" is important here and in the NamedTemporaryFile() call above.
+    # We don't want to change line endings in a file which we're editing.
+    for linenum, line in enumerate(open(fullpath, encoding="utf-8", newline=""), 1):
         if line.strip().startswith("```"):
             if  blockstate == Codeblock.IN_WITH_LANG:
                 blockstate = Codeblock.OUT
+                if EDIT_FILES: temp.write(line)
             elif blockstate == Codeblock.IN:
                 blockstate = Codeblock.OUT
-                if code:
-                    indent = len(fence) - len(fence.lstrip())
-                    code_str = "".join(line[indent:] for line in code)
-                    print(f"{fullpath}:{linenum - len(code) - 1}", file=result)
-                    print(f"```{guess_language(code_str)}\n" + code_str + "```", file=result)
-                    print(file=result)
-                    code = []
+                indent = len(fence) - len(fence.lstrip())
+                code_str = "\n".join(line[indent:].strip() for line in code) + "\n"
+                lang = guess_language(code_str) if code_str else ""
+                if EDIT_FILES:
+                    # When editing files, txt is not very useful edit.
+                    if lang == "txt": lang = ""
+                    temp.write(fence.replace("```", f"```{lang}"))
+                    temp.writelines(code)
+                    temp.write(line)
+                elif lang:
+                    print(f"{fullpath}:{linenum - len(code) - 1}")
+                    print(f"```{lang}\n" + code_str + "```")
+                    print()
+                code = []
             elif line.strip() == "```":
                 blockstate = Codeblock.IN
                 fence = line
             else:
                 blockstate = Codeblock.IN_WITH_LANG
+                if EDIT_FILES: temp.write(line)
         elif blockstate == Codeblock.IN:
             code.append(line)
+        elif EDIT_FILES:
+            temp.write(line)
+    if EDIT_FILES:
+        if code:
+            # non-terminated fence
+            temp.write(fence)
+            temp.writelines(code)
+        temp.flush()
+        copy(temp.name, fullpath)
+        temp.seek(0)
+        temp.truncate(0)
 
-result.close()
+temp.close()
+unlink(temp.name)
